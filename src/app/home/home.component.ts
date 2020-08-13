90b7bb8b6d122a19b9a61f752fd2ce5b76f1d789
Rikesh Lal Shrestha <rikesh.shrestha@citytech.global>
2020-08-13 10:12:08 +0545

    testing file

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,22 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  themeColor = 'magenta';
   welcomeText = 'hello master class';
-  courseLessons=[
-    {title:"course book number 1"},
-    {title:"course book number 2"},
-    {title:"course book number 3"},
-    {title:"course book number 4"},
-    {title:"course book number 5"},
-    {title:"course book number 6"},
-    {title:"course book number 7"},
-    {title:"course book number 8"},
-  ]
+  selectedLesson = undefined;
+  courseLessons = [
+    {title: 'course book number 1'},
+    {title: 'course book number 2'},
+    {title: 'course book number 3'},
+    {title: 'course book number 4'},
+    {title: 'course book number 5'},
+    {title: 'course book number 6'},
+    {title: 'course book number 7'},
+    {title: 'course book number 8'},
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
+  }
+
+  updateColor(){
+    this.themeColor='salmon'
+  }
+
+  lessonSelector(lesson){
+    this.selectedLesson = lesson
   }
 
 }
